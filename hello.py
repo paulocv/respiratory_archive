2eca761d1fbe21f5806569d36d3a41761db04873
Paulo Cesar Ventura <42674294+paulocv@users.noreply.github.com>
2024-11-22 11:52:35 -0500

    Import argparse and pandas

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
 """Goal: test GitHub workflows to run python"""
 
+import argparse
+
+import pandas as pd
+
+# ----------
+
 print("Hello world from this script.")
+
+df = pd.DataFrame([[1, 2, 3], [4, 5, 1000]])
+print(df)

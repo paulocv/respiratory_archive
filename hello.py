1745719b8e9209a58d54197b3c0e5db5a72c082e
Paulo Cesar Ventura <42674294+paulocv@users.noreply.github.com>
2024-11-22 15:33:24 -0500

    Write a file with actions

--- hello.py
+++ hello.py
@@ -10,3 +10,7 @@ print("Hello world from this script.")
 
 df = pd.DataFrame([[1, 2, 3], [4, 5, 1000]])
 print(df)
+
+# --- Write current date and time into a file
+with open("datasets/hello_out.txt", "w") as fp:
+    fp.write(f"Hello. It's {pd.Timestamp.now().isoformat()}\n")
